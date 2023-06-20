7b18f606854d6def295d3fdbe6d64d57de53f453
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-06-20 12:43:04 +0200

    :construction: [Player] Create constructor
    Create constructor of Player class and add initialLife value

--- worldOfCode/classes/Player.ts
+++ worldOfCode/classes/Player.ts
@@ -1,8 +1,19 @@
 export default class Player {
     private _life: number;
+    private _initialLife: number;
     private _strength: number;
     private _experience: number;
     private _weapons: string[];
     private _level: number;
     private _name: string;
+
+    constructor(name: string, life: number, strength: number) {
+        this._name = name;
+        this._life = life;
+        this._initialLife = life;
+        this._strength = strength;
+        this._experience = DEFAULT_EXPERIENCE;
+        this._weapons = DEFAULT_WEAPONS;
+        this._level = DEFAULT_LEVEL;
+    }
 }
