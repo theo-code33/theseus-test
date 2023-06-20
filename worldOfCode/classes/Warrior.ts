c1847edd05a9371a0a9e4bbcff549bb184c7078f
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-06-20 12:51:24 +0200

    :sparkles: [Warrior] Create constant and constructor
    Create constant of Warrior player and pass it to Player class

--- worldOfCode/classes/Warrior.ts
+++ worldOfCode/classes/Warrior.ts
@@ -1,5 +1,10 @@
 import Player from "./Player";
 
+const WARRIOR_LIFE = 120;
+const WARRIOR_STRENGTH = 10;
+
 export default class Warrior extends Player {
-  
+  constructor(name: string) {
+    super(name, WARRIOR_LIFE, WARRIOR_STRENGTH);
+  }
 }
