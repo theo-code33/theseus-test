dc10d8f388bec306e976baad04acb878aff6a31e
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-06-20 12:45:40 +0200

    :sparkles: [Player] Create attack method and _isDead method
    Create attack method to attack opponent and create _isDead method to check if opponent is dead

--- worldOfCode/classes/Player.ts
+++ worldOfCode/classes/Player.ts
@@ -16,4 +16,25 @@ export default class Player {
         this._weapons = DEFAULT_WEAPONS;
         this._level = DEFAULT_LEVEL;
     }
+    private _isDead(player: Player) {
+        this._experience += EXPERIENCE_POINTS_PER_KILL;
+        console.log(`${player._name} is dead!`);
+        if(this._experience >= EXPERIENCE_TO_LEVEL_UP) {
+            this._level++;
+            this._experience = 0;
+            console.log(`${this._name} has leveled up!`);
+        }
+    }
+
+    attack(player: Player) {
+        const damage = this._strength + Math.floor(Math.random() * MAX_ATTACK_DAMAGE_SUPPLEMENTARY);
+        if(player._life - damage <= 0) {
+            player._life = player._initialLife;
+            this._isDead(player);
+        }else{
+            player._life -= damage;
+        }
+        console.log(`${this._name} attacks ${player._name} and inflicts ${damage} damage`);
+    }
+
 }
