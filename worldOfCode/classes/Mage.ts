70cddbe45b45e6240b0786f0d8a95371e60cd774
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-06-20 12:50:32 +0200

    :sparkles: [Mage] Create constant and constructor
    Create constant of Mage player and pass it to Player class

--- worldOfCode/classes/Mage.ts
+++ worldOfCode/classes/Mage.ts
@@ -1,5 +1,10 @@
 import Player from "./Player";
 
-export default class Mage extends Player {
+const MAGE_LIFE = 100;
+const MAGE_STRENGTH = 20;
 
+export default class Mage extends Player {
+    constructor(name: string) {
+        super(name, MAGE_LIFE, MAGE_STRENGTH);
+    }
 }
